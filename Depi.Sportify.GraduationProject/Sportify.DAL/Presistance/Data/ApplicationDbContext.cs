fd220f63a2a6e3cb223a074e6e9258072d429e0f
hassansaiedhassan <hsaidh123@gmail.com>
2025-03-23 12:14:19 +0200

    intilizeProject

---
+++ Depi.Sportify.GraduationProject/Sportify.DAL/Presistance/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sportify.DAL.Presistance.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+    }
+}
