6f16181fdd7f12161f2d6b968d0f0f00724e147a
Nour8317 <noure8317@gmail.com>
2025-03-27 03:03:51 +0200

    initial

---
+++ Sportify/Sportify/Models/LeagueTeam.cs
@@ -0,0 +1,10 @@
+﻿namespace Sportify.Models
+{
+    public class LeagueTeam
+    {
+        public int LeagueId { get; set; }
+        public League League { get; set; }
+        public int TeamId { get; set; }
+        public Team Team { get; set; }
+    }
+}
