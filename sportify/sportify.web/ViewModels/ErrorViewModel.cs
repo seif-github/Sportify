dc300b72d7ce41822bcbc239e7a26c80d1b2f088
seif <sseif9709@gmail.com>
2025-05-03 20:39:43 +0300

    more enhanced?

--- sportify/sportify.web/ViewModels/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace sportify.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
