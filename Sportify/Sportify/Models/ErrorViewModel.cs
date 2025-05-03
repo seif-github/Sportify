e483cb8e1c13c58a6d7c53a1cbbc85e85bb5d426
seif <sseif9709@gmail.com>
2025-05-03 19:45:30 +0300

    Restructure the project

--- Sportify/Sportify/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Sportify.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
